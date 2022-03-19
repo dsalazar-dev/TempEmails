0d5bcac04810048585ee7d7088eaf833b834f4a2
Daniel Salazar <salarm.daniel@outlook.com>
2022-03-19 19:01:41 -0400

    Update Script

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from tempMail2 import TempMail
 
+#Generate a temporary email address with custom domain
+
+temporaryMail = TempMail(login='daniel', domain='@dev.temp')
+print temporaryMail.get_mailbox()  # List emails
+
+#Generate a temporary email address with random domain
+
+temporaryMail = TempMail()
+email = temporaryMail.get_email_address()
+print temporaryMail.get_mailbox(email)  # List emails
